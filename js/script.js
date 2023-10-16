33182fa16137c4eb9d7a5a9c268f2cd5a35afae0
Ryan <linhtruong0690@gmail.com>
2023-10-16 12:35:32 +0700

    update

--- js/script.js
+++ js/script.js
@@ -16,15 +16,12 @@ const sr = ScrollReveal ({
 sr.reveal('.hero-text',{delay:200, origin:'top'});
 sr.reveal('.hero-img',{delay:450, origin:'right'});
 sr.reveal('.icon',{delay:500, origin:'left'});
-sr.reveal('.scroll-down',{delay:500, origin:'right'});
+sr.reveal('.scroll-down button',{delay:400, origin:'right'});
 sr.reveal('.social',{delay:400, origin:'bottom'});
 sr.reveal('.button',{delay:500, origin:'bottom'});
 sr.reveal('.logo',{delay:500, origin:'left'});
 
 function scrollToBottom() {
-	var button = document.getElementById("scroll-down");
-    button.style.opacity = 0.5;
-
     window.scrollTo({
         top: document.body.scrollHeight,
         behavior: 'smooth'
